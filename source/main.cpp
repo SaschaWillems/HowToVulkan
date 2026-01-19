90bcf0b1d59259017efffc3606ba8594713a91b8
Xo Wang <xo@geekshavefeelings.com>
2026-01-18 18:03:40 -0800

    Add SFML Vulkan availability check
    If SFML is built without Vulkan support, find out as early as possible
    rather than fail at surface creation.

--- source/main.cpp
+++ source/main.cpp
@@ -109,9 +109,11 @@ static inline void chk(bool result) {
 int main(int argc, char* argv[])
 {
 	volkInitialize();
+	chk(sf::Vulkan::isAvailable(true));
 	// Instance
 	VkApplicationInfo appInfo{ .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO, .pApplicationName = "How to Vulkan", .apiVersion = VK_API_VERSION_1_3 };
 	const std::vector<const char*> instanceExtensions{ sf::Vulkan::getGraphicsRequiredInstanceExtensions() };
+	chk(!instanceExtensions.empty());
 	VkInstanceCreateInfo instanceCI{
 		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
 		.pApplicationInfo = &appInfo,
